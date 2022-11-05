2779e11fe030ef2344bca7d44ca045b86e2b9365
rannikkopyry <pyry.rannikko1@gmail.com>
2022-11-05 06:28:34 +0200

    Download react infinite carousel

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,38 +1,61 @@
 import React from "react";
 
-const images = [
+const slideImages = [
     {
-        src: "https://i.stack.imgur.com/kyKz5.png"
+      url: 'https://miro.medium.com/max/400/1*nP2C50GK4_-ly_R_mq3juQ.png',
+      caption: 'GraphQL'
     },
     {
-        src: "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png"
-    }
-]
+      url: 'https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png',
+      caption: 'AWS'
+    },
+    {
+      url: 'https://i.stack.imgur.com/kyKz5.png',
+      caption: 'React'
+    },
+  ];
 
 export default function Slider() {
     return (
+        <>
       <div className=" flex items-center min-h-screen justify-center">
         {/* 1. */}
         <div className="w-[200%] h-20 border-t border-b border-gray-600 overflow-hidden relative">
           {/* 2. */}
           <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-20 animate">
             {/* 3 */}
-            {images.map((i) => {
-              return (
-                <div className="flex justify-center items-start w-[20rem]">
-                  <img key={i} src={i}/>
-                </div>
-              );
-            })}
-            {images.map((i) => {
-              return (
-                <div className="flex justify-center items-start w-[20rem]">
-                  <img key={i} src={i}/>
-                </div>
-              );
-            })}
+            {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+            {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
           </div>
         </div>
       </div>
+
+      <style jsx>{`
+        @keyframes scroll {
+            0% {
+              left: 0;
+            }
+            100% {
+              left: -100%;
+            }
+          }
+          
+          .animate {
+            animation: scroll 15s linear infinite;
+          }
+      `}</style>
+    </>
     );
   }
