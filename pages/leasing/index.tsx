138f7df0c1b9cec7ffe43d2a860f607e5b85061f
rannikkopyry <pyry.rannikko1@gmail.com>
2023-04-27 14:02:08 +0300

    Edit price for leasing

--- pages/leasing/index.tsx
+++ pages/leasing/index.tsx
@@ -8,7 +8,6 @@ const index = () => (
     <section className="section">
       <Base>
         <Center className="vertical-center">
-          
           <h1>Leasing-verkkosivut</h1>
           <article className="section__article">
             <p>Uusi tapa uudistaa yrityksesi ilme verkossa.</p>
@@ -29,24 +28,35 @@ const index = () => (
         hankkimaan sivuston kiinteään kuukausihintaan.
       </p>
       <h3>Hinta?</h3>
-      <p>Leasing-verkkosivusto alkaen 135€/kk</p>
+      <p>Leasing-verkkosivusto alkaen 129€/kk</p>
       <h3>Mitä sisältää?</h3>
       <p>
         Jokainen sivu räätälöidään asiakkaan toiveiden mukaan, mutta jokaiseen
         projektiin sisältyy vähintään:
       </p>
       <div className="items">
-      <p className="item"><DoneOutlineIcon /> Verkkosivun suunnittelu ja toteutus </p>
-      <p className="item"><DoneOutlineIcon /> Sivuston kattava ylläpito</p>
-      <p className="item"><DoneOutlineIcon /> Sähköpostipalvelu</p>
-      <p className="item"><DoneOutlineIcon /> SSL-sertifikaatti</p>
-      <p className="item"><DoneOutlineIcon /> Hakukoneoptimointi</p>
-    </div>
-    <div className='link'><a href="/">Palaa etusivulle</a></div>
+        <p className="item">
+          <DoneOutlineIcon /> Verkkosivun suunnittelu ja toteutus{' '}
+        </p>
+        <p className="item">
+          <DoneOutlineIcon /> Sivuston kattava ylläpito
+        </p>
+        <p className="item">
+          <DoneOutlineIcon /> Sähköpostipalvelu
+        </p>
+        <p className="item">
+          <DoneOutlineIcon /> SSL-sertifikaatti
+        </p>
+        <p className="item">
+          <DoneOutlineIcon /> Hakukoneoptimointi
+        </p>
+      </div>
+      <div className="link">
+        <a href="/">Palaa etusivulle</a>
+      </div>
     </section>
 
     <style jsx>{`
-
       h3 {
         font-weight: bold;
       }
